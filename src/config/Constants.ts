8a28d42dbb9e344342905b3544915df6a58fdde1
Maicon Nunes <88409912+MaiconNunes315@users.noreply.github.com>
2021-11-29 18:36:54 -0300

    Add files via upload

---
+++ src/config/Constants.ts
@@ -0,0 +1,7 @@
+
+
+export const config = {
+    API_URL: "http://localhost:5000",
+    MONGO_CONNECTION:
+    "mongodb+srv://shorten:8QbfjDOeB@cluster0.kv7hh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+}
